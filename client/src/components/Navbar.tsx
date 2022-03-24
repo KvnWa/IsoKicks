6e4c967392435d685009f70b56de7aa145858118
Kevin Wang <kevqwang@gmail.com>
2022-03-24 09:46:39 -0400

    test2

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Navbar.css'
+import { Link } from 'react-router-dom';
+import cart from './images/cart.svg'
+import exit from './images/exit.svg'
+import fav from './images/fav.svg'
+import search from './images/search.svg'
+import user from './images/user.svg'
+
+const Navbar = () => {
+    return (
+        <header className="Navbar">
+            <img src={search}></img>
+            <img src={fav}></img>
+            <h1 className="navbar-logo">Iso Kicks</h1>
+            <img src={user}></img>
+            <img src={cart}></img>
+            
+        </header>
+    )
+}
+
+export default Navbar
