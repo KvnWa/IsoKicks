b71a2b6831838881d4aa0b43875ddeb80fbdd58c
Kevin Wang <kevqwang@gmail.com>
2022-03-25 12:31:57 -0400

    landing skeleton

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import mocha from '../images/mocha.jpeg'
+import ald from '../images/ald.jpeg'
+import './LandingPage.css'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className="splash-container">
+      <img src={ald} className="splash-image"></img>
+      <div className="tblock">
+        <div className="oblock">
+          <img src={mocha}></img>
+          <h2>Category</h2>
+            <p className="cat-desc">All domestic orders are shipped via UPS and all international orders are shipped via DHL. This item ships in 5-7 business days. All packages are insured and trackable. An email containing the tracking number will be sent to you when the order ships.</p>
+        </div>
+        <div className="oblock">
+          <img src={ald}></img>
+          <h2>Category</h2>
+          <p className="cat-desc">All domestic orders are shipped via UPS and all international orders are shipped via DHL. This item ships in 5-7 business days. All packages are insured and trackable. An email containing the tracking number will be sent to you when the order ships.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
