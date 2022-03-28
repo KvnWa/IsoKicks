7913e61a6e3e5c0e1e929d0c055255883a5bf473
Kevin Wang <kevqwang@gmail.com>
2022-03-28 13:22:41 -0400

    serializers

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,6 @@
 class Listing < ApplicationRecord
+
+  validates :sneaker_id, :size, presence: true
   belongs_to :sneaker
   has_one :cart_item
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,10 @@
 class User < ApplicationRecord
-    validates :email, presence: true, uniqness: true
-    validates :password, length: (minimum: 3), allow_nil: true
-    validates :password_digest, presence: true
-    validates :session_token, presence: true, uniqness: true
-
-    attr_reader :password
-    after_initialize :ensure_session_token
-
+    has_secure_password
     has_many :cart_items
 
-  
+    validates :email, presence: true, uniqueness: true
+    validates :password, length: (minimum: 3), allow_nil: true
+    validates :password_digest, presence: true
+    validates :session_token, presence: true, uniqueness: true
+    
 end
