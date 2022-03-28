0752aceb25691b094231ed6589b472b6808e0aa1
Kevin Wang <kevqwang@gmail.com>
2022-03-28 13:14:13 -0400

    controllers

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,2 +1,23 @@
 class CartItemsController < ApplicationController
+
+    def create
+        cart_item = CartItem.create!(cart_item_params)
+        render json: cart_item
+    end
+
+    def index
+        cart_items = current_user.cart_items
+        render json: cart_items
+    end
+
+    def destroy
+        cart_item = CartItem.find_by(id: params[:id])
+        cart_item.destroy
+    end
+
+    private
+    
+    def cart_item_params
+        params.require(:cart_item).permit(:user_id, :listing_id)
+    end
 end
