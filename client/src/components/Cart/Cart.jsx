85d0ea81f9dd0f433fd3a2e3c6a5b08497cc2cd3
Kevin Wang <kevqwang@gmail.com>
2022-03-29 20:24:10 -0400

    test

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Cart = () => {
+
+  const [ sneakers, setSneakers ] = useState(null)
+
+
+  const fetchData = async () => {
+    try {
+      const resp = await axios.get('/sneakers');
+      setSneakers(resp.data);
+    } catch(err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>Cart</div>
   )
