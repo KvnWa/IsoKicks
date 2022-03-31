ff576beaae871ddefca11d22da3b760bf684354a
Kevin Wang <kevqwang@gmail.com>
2022-03-30 23:15:50 -0400

    almost finish landing styling

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import mocha from '../images/mocha.jpeg'
 import ald from '../images/ald.jpeg'
 import './LandingPage.css'
@@ -6,7 +6,7 @@ import Slider from '../Slider/Slider'
 import axios from 'axios'
 
 
-const LandingPage = ({sneakers}) => {
+const LandingPage = ({ sneakers }) => {
 
 
 
@@ -19,27 +19,34 @@ const LandingPage = ({sneakers}) => {
             Sacai x Nike
           </div>
           <div className="data-desc">
-          Japan-based fashion house Sacai presents its avant-garde styling to create bold, layered versions of classic Nike models with a “doubled-up” effect. Find all of the sought-after Sacai x Nike collaborations here.
+            Japan-based fashion house Sacai presents its avant-garde styling to create bold, layered versions of classic Nike models with a “doubled-up” effect. Find all of the sought-after Sacai x Nike collaborations here.
           </div>
           <button className="data-button">Shop Now</button>
         </div>
-      <img src='https://www.stadiumgoods.com/BWStaticContent/54000/a31867eb-ef82-4774-9a03-1a257788969b_220328-dd1875-001-desktop-1.jpg' className="splash-image"></img>
+        <img src='https://www.stadiumgoods.com/BWStaticContent/54000/a31867eb-ef82-4774-9a03-1a257788969b_220328-dd1875-001-desktop-1.jpg' className="splash-image"></img>
       </div>
       <div className="tblock">
         <div className="oblock">
-          <img src={mocha}></img>
-          <h2>Category</h2>
-          <p className="cat-desc">All domestic orders are shipped via UPS and all international orders are shipped via DHL. This item ships in 5-7 business days. All packages are insured and trackable. An email containing the tracking number will be sent to you when the order ships.</p>
-          <button>Shop</button>
+          <img src="https://www.stadiumgoods.com/BWStaticContent/54000/cfff8cbc-6969-4127-bd24-a1c4a8688fdb_syntk-air-max-1-module-1000x750.jpg"></img>
+          <div className="journal">
+            <h5 className="red-text">Journal</h5>
+            <h4>Sneakers You Need to Know: Nike Air Max 1</h4>
+            <p className="cat-desc">Get educated on the most iconic Nike running shoe of all time with a guide to its history, colorways, collaborations, sizing info and more.</p>
+            <a className="red-text">Read More</a>
+          </div>
         </div>
         <div className="oblock">
-          <img src={ald}></img>
-          <h2>Category</h2>
-          <p className="cat-desc">All domestic orders are shipped via UPS and all international orders are shipped via DHL. This item ships in 5-7 business days. All packages are insured and trackable. An email containing the tracking number will be sent to you when the order ships.</p>
-          <button>Shop</button>
+          <img src='https://www.stadiumgoods.com/BWStaticContent/54000/b0526877-f625-4088-bbff-a7d04e8e2fbb_whm-tiffanythompson-module.jpg'></img>
+          <div className="journal">
+            <h5 className="red-text">Journal</h5>
+            <h4>Celebrating Women’s History Month: Interview With Tiffany Thompson</h4>
+            <p className="cat-desc">The renowned interior designer spoke to us about her journey from Nike to her own design business, who inspires her, and of course, her favorite sneakers.
+            </p>
+            <a className="red-text">Read More</a>
+          </div>
         </div>
       </div>
-      <Slider sneakers={sneakers}/>
+      <Slider sneakers={sneakers} />
     </div>
   )
 }
--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -4,7 +4,7 @@ import mocha from "../images/mocha.jpeg"
 import './Slider.css'
 import axios from 'axios';
 
-const Slider = ({sneakers}) => {
+const Slider = ({ sneakers }) => {
 
 
     return (
@@ -12,24 +12,29 @@ const Slider = ({sneakers}) => {
             <h1 className="sli-bottom">Top Kicks</h1>
             <div className="slider">
                 <figure className="sli-c">
-                    <img src={ald}>
-                    </img>
+                    <a href="/sneakers/1">
+                        <img src="https://img.stadiumgoods.com/15/95/78/61/15957861_29809198_2048.jpg" />
+                    </a>
                 </figure>
                 <figure className="sli-c">
-                    <img src={mocha}>
-                    </img>
+                    <a href="/sneakers/27">
+                        <img src="https://img.stadiumgoods.com/16/81/96/45/16819645_34397436_2048.jpg" />
+                    </a>
                 </figure>
                 <figure className="sli-c">
-                    <img src={ald}>
-                    </img>
+                    <a href="/sneakers/9">
+                        <img src="https://img.stadiumgoods.com/12/96/02/38/12960238_34435897_2048.jpg" />
+                    </a>
                 </figure>
                 <figure className="sli-c">
-                    <img src={ald}>
-                    </img>
+                    <a href="/sneakers/6">
+                        <img src="https://img.stadiumgoods.com/jordan-kids-air-jordan-1-retro-high-og-bg-unc_13157688_34424154_2048.jpg" />
+                    </a>
                 </figure>
                 <figure className="sli-c">
-                    <img src={ald}>
-                    </img>
+                    <a href="/sneakers/14">
+                        <img src="https://img.stadiumgoods.com/adidas-yeezy-yeezy-boost-700-utility-black_14244732_34498013_2048.jpg" />
+                    </a>
                 </figure>
             </div>
         </div>
